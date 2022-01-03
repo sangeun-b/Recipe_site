a7456306ed51d883537e9b4463124e8c125ed9db
jeonghyunkyu <jeonghyunkyu@218.38.137.27>
2022-01-03 16:42:53 +0900

    테스트입니

---
+++ spring_cooking_recipes/src/main/java/com/encore/demo/SpringCookingRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.encore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCookingRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCookingRecipesApplication.class, args);
+	}
+
+}
---
+++ spring_cooking_recipes/src/main/java/com/encore/demo/board/board.java
@@ -0,0 +1,5 @@
+package com.encore.demo.board;
+
+public class board {
+
+}
