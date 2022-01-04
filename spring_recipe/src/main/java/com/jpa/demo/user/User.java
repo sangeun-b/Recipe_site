4272468cec73aa68d689805d39be73fde6f2ec5a
sangeun-b <baek0007@algonquinlive.com>
2022-01-04 17:25:18 +0900

    view

--- spring_recipe/src/main/java/com/jpa/demo/user/User.java
+++ spring_recipe/src/main/java/com/jpa/demo/user/User.java
@@ -1,6 +1,30 @@
 package com.jpa.demo.user;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+	
+	@Id
+	private String id;
+	@Column(nullable = false)
+	private String pwd;
+	@Column(nullable = false)
+	private String email;
+	
 	
 }
---
+++ spring_recipe/src/main/java/com/jpa/demo/user/UserService.java
@@ -0,0 +1,23 @@
+package com.jpa.demo.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserDao dao;
+	
+	public User saveUser(User u) {
+		return dao.save(u);
+	}
+	
+	public User getUser(String id) {
+		return dao.findById(id).orElse(null);
+	}
+	
+	public void delUser(String id) {
+		dao.deleteById(id);
+	}
+}
