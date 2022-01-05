3d4f79a9996164efb1ee13abe7cfc6ccc3cee163
Playdata <Playdata@192.168.200.180>
2022-01-05 17:19:31 +0900

    test

--- spring_recipe/src/main/java/com/jpa/demo/user/User.java
+++ spring_recipe/src/main/java/com/jpa/demo/user/User.java
@@ -1,6 +1,17 @@
 package com.jpa.demo.user;
 
+@Entity  
+@Setter
+@Getter
+@ToString
 public class User {
 
+	@Id
+	private String id;
 	
+	@Column(nullable=false)
+	private String pwd;
+	
+	@Column(nullable=false)
+	private String email;
 }
