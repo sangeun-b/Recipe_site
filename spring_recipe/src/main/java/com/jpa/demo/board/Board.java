32a02f3439cdcf4c8935852af81061afc51b0378
Playdata <Playdata@192.168.200.180>
2022-01-11 15:01:24 +0900

    보드쓰기

--- spring_recipe/src/main/java/com/jpa/demo/board/Board.java
+++ spring_recipe/src/main/java/com/jpa/demo/board/Board.java
@@ -49,9 +49,10 @@ public class Board {
 	private Date date;
 	private String difficulty;
 	private String cate;
+	private String ingredient;
 	
-	@Transient
-	private ArrayList img_path; //이미지
+	
+	private String img_path; //이미지
 	
 	@PrePersist//insert 실행전 먼저 처리
 	public void beforeCreate() {
